d4c20efdce8e6e76fe729899d0d655c9f200fe39
BikmanDjuma <bikmangeek@gmail.com>
2024-08-25 00:29:47 +0200

    first commit

--- resources/views/auth/self_registration.blade.php
+++ resources/views/auth/self_registration.blade.php
@@ -5,7 +5,8 @@
               <div class="card auth-form-light">
                 <div class="card-body">
                 <div class="brand-logo align-items-center text-center">
-                    <img src="{{ URL::to('/') }}/style/images/logo.png" alt="logo">
+                    <!-- <img src="{{ URL::to('/') }}/style/images/logo.png" alt="logo"> -->
+                    <img class="img-fluid p-1" src="{{ URL::to('/') }}/homePage/img/logo.jpeg" alt="" width="200" height="40"> 
                 </div>
                     
                     @if($errors->any())
