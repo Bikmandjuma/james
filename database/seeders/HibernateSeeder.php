3220b7dc9062081d37a60c27a353412bc4d9e286
BikmanDjuma <bikmangeek@gmail.com>
2024-08-26 11:15:54 +0200

    changes

--- database/seeders/HibernateSeeder.php
+++ database/seeders/HibernateSeeder.php
@@ -51,7 +51,7 @@ class HibernateSeeder extends Seeder
             ],[
                 'id' => $question_id + 1,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'Which of the following is a core interface of Hibernate?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
@@ -63,43 +63,43 @@ class HibernateSeeder extends Seeder
             ],[
                 'id' => $question_id + 3,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'What is the function of the Session interface in Hibernate?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
                 'id' => $question_id + 4,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'Which annotation is used to map a Java class to a database table in Hibernate?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
                 'id' => $question_id + 5,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'What is the purpose of the @Id annotation in Hibernate?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
                 'id' => $question_id + 6,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'What does the lazy attribute of the @OneToMany annotation control in Hibernate?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
                 'id' => $question_id + 7,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'How does Hibernate handle transactions?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
                 'id' => $question_id + 8,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'Which of the following is true about Hibernate Query Language (HQL)?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
                 'id' => $question_id + 9,
                 'exam_id' => $exam_id,
-                'question_text' => 'What is Hibernate in Java?',
+                'question_text' => 'What is the purpose of the Criteria API in Hibernate?',
                 'question_type' => 'multiple-choice',
                 'marks' => '10',
             ],[
@@ -113,29 +113,29 @@ class HibernateSeeder extends Seeder
             // Insert options individually
             $options = [
                 [
-                    'id' => 81,
-                    'question_id' => 32,
+                    'id' => $option_id,
+                    'question_id' => $question_id,
                     'option_text' => 'A database management system',
                     'is_correct' => 'false',
                 ],
                 [
-                    'id' => 82,
-                    'question_id' => 32,
+                    'id' => $option_id + 1,
+                    'question_id' => $question_id + 1,
                     'option_text' => 'A Java framework for building web applications',
                     'is_correct' => 'false',
                 ],
                 [
-                    'id' => 83,
-                    'question_id' => 32,
+                    'id' => $option_id + 2,
+                    'question_id' => $question_id + 2,
                     'option_text' => 'An ORM (Object-Relational Mapping) framework',
                     'is_correct' => 'true',
                 ],
                 [
-                    'id' => 84,
-                    'question_id' => 32,
+                    'id' => $option_id + 3,
+                    'question_id' => $question_id + 3,
                     'option_text' => 'A Java library for handling HTTP requests',
                     'is_correct' => 'false',
-                ]
+                ],
             ];
 
             Option::insert($options);
